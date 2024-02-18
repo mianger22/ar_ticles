e8a6302c59c3a527d5d5ae8658def0edb4711f05
mianger22 <test666777888999@yandex.ru>
2024-02-18 10:18:19 +0300

    Added tailwind and columns main page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <>
-      hello
+      <header></header>
+      <main className="grid-container grid grid-cols-9 gap-2 h-screen">
+        <div className="item1 col-span-2 border-2 border-gray-600 border-solid"></div>
+        <div className="item2 col-span-5 border-2 border-gray-600 border-solid flex justify-center">
+          <h1 className="text-3xl font-bold underline">
+            Итак, начнём
+          </h1></div>
+        <div className="item2 col-span-2 border-2 border-gray-600 border-solid"></div>
+      </main>
+      <footer></footer>
     </>
   );
 }
