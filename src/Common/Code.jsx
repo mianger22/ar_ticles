b8fa494db1a733ee09a61b19067eea11c7a5684f
mianger22 <test666777888999@yandex.ru>
2024-02-26 18:31:06 +0300

    I made a reusable block of code

---
+++ src/Common/Code.jsx
@@ -0,0 +1,9 @@
+function Code({ content }) {
+    return (
+        <span className="bg-amber-400">
+            {content}
+        </span>
+    )
+}
+
+export default Code;
--- src/Main/CenterBlock/CenterBlock.jsx
+++ src/Main/CenterBlock/CenterBlock.jsx
@@ -1,3 +1,66 @@
+import Code from "../../Common/Code";
+
+const Codes = {
+    1: `<!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Our first AR application</title>
+    </head>
+    <body>
+        // Тут будет наш код
+    </body>
+    </html>`,
+    2: `<meta name="viewport" content="width=device-width, 
+    user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">`,
+    3: `<meta name="viewport" content="width=device-width, 
+    initial-scale=1.0">`,
+    4: `<!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Our first AR application</title>
+    </head>
+    <body>`,
+    5: `<script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+        <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+        // Мы подключили библиотеку AR.js
+    </body>
+    </html>`,
+    6: `// Мы подключили библиотеку AR.js
+    </body>
+    </html>`,
+    7: `<a-scene embedded arjs>
+    <a-marker preset="hiro">
+        <a-text value="YA LUBLU TEBYA, MALISH :*" color="blue" scale="2 2 1"></a-text>
+    </a-marker>
+    <a-entity camera></a-entity>
+    </a-scene>`,
+    8: `<!DOCTYPE html>
+        <html lang="ru">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Our first AR application</title>
+        </head>
+        <body>
+            <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+            <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+            <a-scene embedded arjs>
+                <a-marker preset="hiro">
+                    <a-text value="YA LUBLU TEBYA, MALISH :*" color="blue" scale="2 2 1"></a-text>
+                </a-marker>
+                <a-entity camera></a-entity>
+            </a-scene>
+        </body>
+        </html>`
+}
+
 function CenterBlock() {
   return (
     <div className="item2 col-span-5 border-2 border-gray-600 border-solid flex justify-center">
@@ -12,19 +75,47 @@ function CenterBlock() {
                 Всё просто.
 
                 1. Создаём стандартный html-файл. 
-                2. Подключаем к нему библиотеку AR.js 
+
+                <div>
+                    <Code content={Codes[1]} />
+                </div>
+                
+                ! Внимание - мы пишем <Code content={Codes[2]} /> 
+                вместо <Code content={Codes[3]} /> для того, чтобы камера была без неприятного и неудобного увеличения
+                
+                2. Подключаем библиотеку AR.js, прописывая соответствующие теги в index.html в разделе body
+                <div>
+                    <Code content={Codes[4]} />
+                    <span className="font-black">
+                        <Code content={Codes[5]} />
+                    </span>
+                    <Code content={Codes[6]} />
+                </div>
+
                 3. Добавляем следующий код
+                <div className="font-black">
+                    <Code content={Codes[7]} />
+                </div>
 
                 Что он делает
 
-                Во-первых, а-сцен создаёт сцену
-                Во-вторых, а-маркер ищет маркер наведённой камерой
+                Во-первых, <b>a-scene</b> создаёт сцену
+                Во-вторых, <b>a-marker</b> ищет маркер наведённой камерой. Его аттрибут <b>preset="hiro"</b> позволяет программе понять - какой маркер ей искать
                 Всё, что внутри - выводит нужное нам содержимое над маркеромю. У нас это текст
 
-                Внимание! Я испрорльзовал этот вместо этого
+                Внимание! Я испрорльзовал <b>a-text</b> вместо этого, для того чтобы текст был крупнее. Вы же можете использовать стандартный а-ембеддед
+
+                В итоге получилось следующий код нашего первого AR-приложения:
+
+копировать
+                <div>
+                    <Code content={Codes[8]} />
+                </div>
 
                 4. Деплоим на гитхаб, меняем настройки, переходим по следующему адресу на созданный github pages c телефона, наводим на этот маркер
                 ожидаем, держа камеру так, чтобы картинка была параллельна
+
+                Можете протестировать мой результат прежде чем возиться со своим, или чтобы сравнить свой с моим
             </div>
         </div>
     </div>
