4c35f8ea5bbfcb2e8514e04a904d3d48119d5b14
mianger22 <test666777888999@yandex.ru>
2024-02-28 13:40:56 +0300

    added a new library devdepending on

--- src/Main/CenterBlock/CenterBlock.jsx
+++ src/Main/CenterBlock/CenterBlock.jsx
@@ -4,7 +4,7 @@ const Codes = {
     1: `
         <!DOCTYPE html>
         <html lang="ru">
-            <head>
+            \u0009<head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
