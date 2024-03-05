a2d941dea519b674a3fab467b7d3836d19b1d0ef
mianger22 <test666777888999@yandex.ru>
2024-03-05 13:19:11 +0300

    Changed copy icon

--- src/Common/CopyButton.jsx
+++ src/Common/CopyButton.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 function CopyButton({ codes }) {
+    const [copied, setCopied] = useState(false);
+
+    const copy = () => {
+        // копируем код
+        navigator.clipboard.writeText(codes);
+        // меняем значок, чтобы показать, что операция выполнена успешно
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 3000);
+    }
+        
     return (
         <div className="uk-card-badge uk-background-default">
-            <button onClick={() => {navigator.clipboard.writeText(codes)}}>
-                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/copy.png" alt="copy code" />
+            <button onClick={copy}>
+                {
+                    copied ? <img width="32" height="32" src="https://img.icons8.com/windows/32/ok--v1.png" alt="ok--v1"/>
+                    : <img width="30" height="30" src="https://img.icons8.com/ios/50/copy--v1.png" alt="copy code" />
+                }
             </button>
         </div>
     )
