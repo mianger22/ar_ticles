184bcd578f3fc137be77f9b9d685e55c28d69653
mianger22 <test666777888999@yandex.ru>
2024-03-09 14:20:01 +0300

    I made an appeal to the API

--- src/Main/CenterBlock/Comments/Comments.jsx
+++ src/Main/CenterBlock/Comments/Comments.jsx
@@ -1,22 +1,76 @@
 import { Formik, Form, Field } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Comment from "./Comment";
+const axios = require('axios');
+const url_resourse = "http://localhost:80/comments";
 
 function Comments() {
     const [comments_list, set_comments_list] = useState([]);
+
+    useEffect(() => {    
+        // Want to use async/await? Add the `async` keyword to your outer function/method.
+        async function getComments() {
+            try {
+                await axios.get(url_resourse)
+                    .then(function (response) {
+                        // Полученные комментарии с сервера сохраняем в переменную comments_list, 
+                        // в следствие чего пользователь увидит все комментарии сайта
+                        set_comments_list(response)
+                    })
+                    .catch(function (error) {
+                        console.log(` Ошибка обращения по адресу: ${error} `);
+                    })
+                    .finally(function () {
+                        // always executed
+                    });  
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getComments();
+    })
 
     return (
         <article className="uk-article uk-margin-large-bottom">
             <Formik
                 initialValues={{ user_name: "", comment: "" }}
                 onSubmit = {(values, { resetForm }) => {
+                    // сохраняем текущую дату
+                    const currentDate = JSON.stringify(new Date(2011, 0, 1, 0, 0, 0, 0));
+
                     // сохраняем имя пользователя, его комментарий и время создания комментария
                     set_comments_list([...comments_list, {
                         id: comments_list.length + 1,
                         user_name: values.user_name, 
-                        time_comment_creation: JSON.stringify(new Date(2011, 0, 1, 0, 0, 0, 0)),
+                        time_comment_creation: currentDate,
                         comment: values.comment
                     }]);
+
+                    // сохраняем в Базу Данных новый комментарий
+                    async function saveComment() {
+                        try {
+                            await axios.post(url_resourse, {
+                                "user_name": values.user_name, "comment": values.comment, "date_creation": currentDate
+                            })
+                                .then(function (response) {
+                                    // Показываем сообщение об успехе
+                                    alert(response);
+                                })
+                                .catch(function (error) {
+                                    alert("Ошибка! Детали - в консоли");
+                                    console.clear();
+                                    console.log(` Ошибка обращения по адресу: ${error} `);
+                                })
+                                .finally(function () {
+                                    // always executed
+                                });  
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    };
+            
+                    saveComment();
 
                     // очищаем поля
                     resetForm({});
