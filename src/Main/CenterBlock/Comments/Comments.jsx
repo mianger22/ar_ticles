2897e9941443f91457f7271ffbfc2eef8d558a7e
mianger22 <test666777888999@yandex.ru>
2024-03-10 08:23:00 +0300

    Changed the name of the button

--- src/Main/CenterBlock/Comments/Comments.jsx
+++ src/Main/CenterBlock/Comments/Comments.jsx
@@ -153,7 +153,7 @@ function Comments() {
                             </div>
                         </fieldset>
 
-                        <button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Спросить</button>
+                        <button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Отправить</button>
                     </Form>
                 )}
             </Formik>         
